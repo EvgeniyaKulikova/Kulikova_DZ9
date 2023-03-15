9324c9fd011b9a9ae9377866241fb4bf9d76c1cc
Evgeniya <kulikova.ev94@gmail.com>
2023-03-15 16:31:23 +0300

    задача64(1)

---
+++ задача64(1)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64(1) задайте значение м и n. напишите программу которая выведет все натуральные числа в промежутке от м до n.
+
+
+
+void che(int m, int n)
+{
+    if (m > n)
+    return;
+    else
+    {
+        Console.Write($"{m}, ");
+    }
+    che(m+1,n);
+    
+}
+
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+che(m,n);
+
+
+
